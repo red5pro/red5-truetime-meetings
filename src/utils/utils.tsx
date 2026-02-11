0b6bb067cdbcacbe132a0b294d6ea77703e077a2
lastpeony <yguney@sabanciuniv.edu>
2026-02-11 14:45:47 +0000

    remove uppercase from ids

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -118,7 +118,7 @@ export function getLang(): string {
 
 export function makeId(length: number): string {
     let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
