b400f7a2571a2031ac151f8a6d9892be90fb17fe
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2026-05-22 22:27:32 +0300

    Add new fields into the config server

--- config-service/src/utils/validation.js
+++ config-service/src/utils/validation.js
@@ -1,8 +1,39 @@
+export const ALLOWED_KEYS = new Set([
+  'VITE_HOST',
+  'VITE_BACKEND_HOST',
+  'VITE_ENABLE_RECORDING',
+  'VITE_ENABLE_CLOSED_CAPTION',
+  'VITE_DEFAULT_THEME',
+  'VITE_TURN_SERVER_URL',
+  'VITE_TURN_SERVER_USERNAME',
+  'VITE_TURN_SERVER_CREDENTIAL',
+  'VITE_NODE_GROUP',
+  'VITE_LOGO_URL',
+  'VITE_BASENAME',
+  'VITE_ENABLE_EXTERNAL_STREAMS',
+  'VITE_VIRTUAL_BACKGROUND_IMAGES',
+  'VITE_GOOGLE_CLIENT_ID',
+  'VITE_ENABLE_GOOGLE_AUTH',
+  'VITE_PUBNUB_PUBLISH_KEY',
+  'VITE_PUBNUB_SUBSCRIBE_KEY',
+  'VITE_AWS_ACCESS_KEY',
+  'VITE_AWS_SECRET_ACCESS_KEY',
+  'VITE_AWS_BUCKET_NAME',
+  'VITE_AWS_BUCKET_LOCATION',
+]);
+
 export const validThemes = ['blue', 'black'];
 export const validBooleans = ['true', 'false'];
 
 export const validateConfig = (config) => {
   const errors = [];
+
+  // Reject unknown keys
+  for (const key of Object.keys(config)) {
+    if (!ALLOWED_KEYS.has(key)) {
+      errors.push(`Unknown config key: ${key}`);
+    }
+  }
 
   // Validate URLs
   if (config.VITE_TURN_SERVER_URL !== undefined && config.VITE_TURN_SERVER_URL !== '') {
